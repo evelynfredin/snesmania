530fd393d468e2f30bb9811174c1e89d6d2783ef
evelynfredin <evefre2001@skola.goteborg.se>
2021-04-07 07:55:28 +0200

    Project setup

---
+++ src/theme/themes.js
@@ -0,0 +1,20 @@
+const themes = {
+  light: {
+    "--highlight": "#5948AB",
+    "--primary": "#585656",
+    "--background": "#F4F5FF",
+    "--hover": "#f47188",
+    "--secondary": "#fff",
+    "--color-light-gray": "#888686",
+  },
+  dark: {
+    "--highlight": "#B8B7E1",
+    "--primary": "#E5E5E5",
+    "--background": "#2C2D37",
+    "--hover": "#f47188",
+    "--secondary": "#302f2f",
+    "--color-light-gray": "#c7c7c7",
+  },
+};
+
+export default themes;
