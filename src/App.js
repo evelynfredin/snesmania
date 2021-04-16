215705cf514b25bbbb37e07596a54410776ee84a
evelynfredin <evefre2001@skola.goteborg.se>
2021-04-16 08:42:13 +0200

    Added function that scrolls to the top after a render

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import themes from './theme/themes';
 import Header from './components/Header';
 import SingleBlog from './pages/SingleBlog';
 import Footer from './components/Footer';
+import ScrollToTop from './functions/ScrollToTop';
 import './App.css';
 
 // Pages
 import Home from './pages/Home';
 
 function App() {
-	const [theme, setTheme] = React.useState('light');
+	const [theme, setTheme] = useState('light');
 	const toggleTheme = () => {
 		const updateTheme = theme === 'light' ? 'dark' : 'light';
 		setTheme(updateTheme);
@@ -19,14 +20,16 @@ function App() {
 	return (
 		<main style={themes[theme]}>
 			<Router>
-				<Header handleClick={toggleTheme} />
-				<Route exact path="/">
-					<Home />
-				</Route>
-				<Route path="/blog/:id">
-					<SingleBlog />
-				</Route>
-				<Footer></Footer>
+				<ScrollToTop>
+					<Header handleClick={toggleTheme} />
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/blog/:id/">
+						<SingleBlog />
+					</Route>
+					<Footer />
+				</ScrollToTop>
 			</Router>
 		</main>
 	);
